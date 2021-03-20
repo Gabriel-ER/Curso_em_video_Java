d9dd3618e2237f8bfa9f52e9c748d0914405d37a
Gabriel Evangelista Rodrigues <gabrielevangelista2.0@gmail.com>
2021-03-20 09:35:31 -0300

    Exercício de java script

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+//Isso é um comentário
+var velocidade = 78.2
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+
+if (velocidade>60) {//condição simples
+    console.log('Você ultrapassou a velocidade permitida! MULTA GERADA')
+}
+
+console.log('O cinto salva vidas!')
